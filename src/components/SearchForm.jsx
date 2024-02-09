65816dd51ab11057058f1e401f2eff2f0a6f2e94
Arian <Arianbod@outlook.com>
2024-02-09 21:11:35 +0300

    created

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchForm = () => {
-	return <h4>SearchForm</h4>;
+	const [searchValue, setSearchValue] = useState('');
+	const handleSearchChange = (e) => setSearchValue(e.target.value);
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(searchValue);
+	};
+	return (
+		<section>
+			<h1 className='title'>Unsplash Images</h1>
+			<form
+				onSubmit={handleSubmit}
+				className='search-form'>
+				<input
+					type='text'
+					name='search'
+					value={searchValue}
+					className='form-input search-input'
+					onChange={handleSearchChange}
+					placeholder='cat'
+				/>
+				<button
+					className='btn'
+					type='submit'>
+					search
+				</button>
+			</form>
+		</section>
+	);
 };
 
 export default SearchForm;
